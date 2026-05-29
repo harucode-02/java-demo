997538916b719aa67cc51da0a337636dd788dcab
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-05-29 19:25:39 +0700

    feat: Object-Oriented Programming

---
+++ src/brocode/OOP/Main.java
@@ -0,0 +1,34 @@
+package brocode.OOP;
+
+public class Main {
+    static void main(String[] args) {
+//        Object = An entity that holds data (attributes)
+//                and can perform actions (methods)
+//        It is a reference data type
+
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        System.out.println(car1.make + " " + car1.model);
+        System.out.println(car2.make + " " + car2.model);
+
+
+//        car.drive();
+//        car.brake();
+
+//        System.out.println(car.isRunning);
+//        car.start();
+//        System.out.println(car.isRunning);
+//        car.stop();
+//        System.out.println(car.isRunning);
+
+//        car.isRunning = true;
+
+//        System.out.println(car.make);
+//        System.out.println(car.model);
+//        System.out.println(car.year);
+//        System.out.println(car.price);
+//        System.out.println(car.isRunning);
+    }
+}
