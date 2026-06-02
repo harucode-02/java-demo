6eae9e5e5748ef9fb8f28d7b7984047fa79fffce
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-02 16:39:08 +0700

    feat: Wrapper Class

---
+++ src/brocode/OOP/Main17.java
@@ -0,0 +1,51 @@
+package brocode.OOP;
+
+public class Main17 {
+    static void main(String[] args) {
+//        Wrapper classes = Allow primitive values (int, char, double, boolean)
+//                          to be used as objects. "Wrap them in an object"
+//                          Generally, don't wrap primitives unless you need an object.
+//                          Allows use of Collections Framework and static Utility Methods.
+
+//        Integer a = new Integer(123);
+//        Double b = new Double(3.14);
+//        Character c = new Character('$');
+//        Boolean d = new Boolean(true);
+
+//        // Autoboxing
+//        Integer a = 123;
+//        Double b = 3.14;
+//        Character c = '$';
+//        Boolean d = true;
+//
+//        // Unboxing
+//        int x = a;
+//        double y = b;
+//        char z = c;
+//        boolean e = d;
+
+//        String a = Integer.toString(123);
+//        String b = Double.toString(3.14);
+//        String c = Character.toString('@');
+//        String d = Boolean.toString(false);
+//
+//        String x = a + b + c + d;
+//
+//        System.out.println(x);
+
+//        int a = Integer.parseInt("123");
+//        double b = Double.parseDouble("3.14");
+//        char c = "Pizza".charAt(0);
+//        boolean d = Boolean.parseBoolean("true");
+//
+//        String x = a + b + c + d;
+//
+//        System.out.println(x);
+
+//        char letter = '$';
+        char letter = 'b';
+
+        System.out.println(Character.isLetter(letter));
+        System.out.println(Character.isUpperCase(letter));
+    }
+}
