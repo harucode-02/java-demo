a11e4531ad16abe8d8acb11ccbcc5bf0bf55c79e
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-03 19:42:16 +0700

    feat: Threading

---
+++ src/brocode/OOP/M31MyRunnable.java
@@ -0,0 +1,21 @@
+package brocode.OOP;
+
+public class M31MyRunnable implements Runnable{
+
+    @Override
+    public void run(){
+        for(int i = 1; i <= 10; i++){
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                System.out.println("Thread was interrupted");
+            }
+            if(i == 10){
+                System.out.println("Time's up!");
+                System.exit(0);
+            }
+        }
+    }
+}
