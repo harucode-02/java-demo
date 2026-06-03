52b28050c50d8d8b84efbcc70ae6c96a4ec55c0f
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-03 17:33:10 +0700

    feat: Hash Map

---
+++ src/brocode/OOP/Main29.java
@@ -0,0 +1,44 @@
+package brocode.OOP;
+
+import java.util.HashMap;
+
+public class Main29 {
+    static void main(String[] args) {
+
+//        HashMap = A data structure that stores key-value pairs
+//                  Keys are unique, but Values can be duplicated
+//                  Does not maintain any order, but is memory efficient
+//                  HashMap<Key, Value>
+
+        HashMap<String, Double> map = new HashMap<>();
+
+        map.put("apple", 0.50);
+        map.put("orange", 0.75);
+        map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+
+//        map.remove("apple");
+//        map.put("orange", 1000000.00);
+
+//        System.out.println(map);
+//        System.out.println(map.get("coconut"));
+//        System.out.println(map.containsKey("pineapple"));
+
+//        if(map.containsKey("pineapple")){
+//            System.out.println(map.get("pineapple"));
+//        }
+//        else{
+//            System.out.println("Key not found!");
+//        }
+
+//        System.out.println(map.containsValue(1.00));
+
+//        System.out.println(map.size());
+
+//        System.out.println(map);
+
+        for(String key : map.keySet()){
+            System.out.println(key + " : $" + map.get(key));
+        }
+    }
+}
