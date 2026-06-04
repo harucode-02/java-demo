ab7e69646de3d1d50df9730b26ca017faa425253
haruhuynh2208@gmail.com <haruhuynh369@gmail.com>
2026-06-04 14:35:58 +0700

    feat: Revision

--- src/brocode/OOP/M33AlarmClock.java
+++ src/brocode/OOP/M33AlarmClock.java
@@ -7,32 +7,29 @@ import java.io.IOException;
 import java.time.LocalTime;
 import java.util.Scanner;
 
-public class M33AlarmClock implements Runnable{
+public class M33AlarmClock implements Runnable {
 
     private final LocalTime alarmTime;
     private final String filePath;
     private final Scanner scanner;
 
-    M33AlarmClock(LocalTime alarmTime, String filePath, Scanner scanner){
+    M33AlarmClock(LocalTime alarmTime, String filePath, Scanner scanner) {
         this.alarmTime = alarmTime;
         this.filePath = filePath;
         this.scanner = scanner;
     }
 
     @Override
-    public void run(){
+    public void run() {
 
-        while(LocalTime.now().isBefore(alarmTime)){
+        while (LocalTime.now().isBefore(alarmTime)) {
             try {
                 Thread.sleep(1000);
-
                 LocalTime now = LocalTime.now();
-
                 System.out.printf("\r%02d:%02d:%02d",
-                                    now.getHour(),
-                                    now.getMinute(),
-                                    now.getSecond());
-
+                        now.getHour(),
+                        now.getMinute(),
+                        now.getSecond());
             } catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
             }
@@ -41,16 +38,17 @@ public class M33AlarmClock implements Runnable{
         System.out.println("\n*ALARM NOISES*");
         playSound(filePath);
     }
-    private void playSound(String filePath){
+
+    private void playSound(String filePath) {
 
         File audioFile = new File(filePath);
 
-        try ( AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)){
+        try (AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)) {
             Clip clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start();
             System.out.print("Press *Enter* to stop the alarm: ");
-            scanner. nextLine();
+            scanner.nextLine();
             clip.stop();
 
             scanner.close();
@@ -59,8 +57,7 @@ public class M33AlarmClock implements Runnable{
             System.out.println("Audio file format is not supported");
         } catch (LineUnavailableException e) {
             System.out.println("Audio is unavailable");
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error reading audio file");
         }
     }
